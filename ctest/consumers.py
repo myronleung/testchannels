8067f552ce226e283a8e51b378551eff441fd0de
Myron Leung <myronleung@gmail.com>
2016-07-06 11:04:46 -0400

    base working

--- ctest/consumers.py
+++ ctest/consumers.py
@@ -1,17 +1,13 @@
 from channels import Group
 from channels.sessions import channel_session
 from ctest.models import *
+import json
 
 # Connected to websocket.receive
 @channel_session
 def ws_connect(message):
-    # Work out room name from path (ignore slashes)
-    room = message.content['path'].strip("/")
-    # Save room in session and add us to the group
-    message.channel_session['room'] = room
-    Group("chat-%s" % room).add(message.reply_channel)
+    Group("chat").add(message.reply_channel)
     print('--- connect ---')
-    print('room: '+room)
 
 # Connected to websocket.receive
 @channel_session
@@ -21,18 +17,20 @@ def ws_message(message):
     if message['text'] != '':
         m = Message(author = u, room = r, message=message['text'])
         m.save()
-    print('message: '+message['text'])
-    try:
-        Group("chat").send({
-            "text": "[user] %s" % message.content['text'],
-        })
-        print('sent')
-    except:
-        print('failed')
+    print('message: '+message.content['text'])
+    data = json.loads(message.content['text'])
+    print('data:')
+    print(data)
+    print('---------')
+    # try:
+    Group("chat").send({'text':m.message})
+    print('sent')
+    # except:
+    #     print('failed')
 
 
 # Connected to websocket.disconnect
 @channel_session
 def ws_disconnect(message):
-    Group("chat-%s" % message.channel_session['room']).discard(message.reply_channel)
+    Group("chat").discard(message.reply_channel)
     print('dc')
